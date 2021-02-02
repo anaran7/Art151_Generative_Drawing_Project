051b6e89206b3d7837f77142f9f5c8b1b6f54c4c
anaran7 <naranjoa101@gmail.com>
2021-02-02 14:15:21 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-//rectangle object
-let rectangle = {
+//eli object
+let eli = {
     x: 0,
     y: 0,
     width: 10, 
@@ -15,72 +15,97 @@ let rectangle = {
 };
 
 let x = 0;
+let maxDiameter; 
+let theta = 0; 
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     background(70, 103, 80);
-
+    maxDiameter = windowWidth; 
     fill(random(0,255), random(0,255), random(0,255));
     updateColors();
 }
 
 function updateColors(){
-    rectangle.color1 = random(0,255);
-    rectangle.color2 = random(0,255);
-    rectangle.color3 = random(0,255);
-    rectangle.color4 = random(0,255);
-    rectangle.color5 = random(0,255);
-    rectangle.color6 = random(0,255);
+    eli.color1 = random(0,255);
+    eli.color2 = random(0,255);
+    eli.color3 = random(0,255);
+    eli.color4 = random(0,255);
+    eli.color5 = random(0,255);
+    eli.color6 = random(0,255);
 }
 
 function draw(){
 
-    strokeWeight(2);
+    //strokeWeight(2);
     //background("#ffffff01");
 
     //the height and the width is decided by mouse input
-    rectangle.width = map(mouseX, 0, windowWidth, -20, 20);
-    rectangle.height = map(mouseY, 0, windowHeight, -100, 100);
+    eli.width = map(mouseX, 0, windowWidth, -20, 20);
+    eli.height = map(mouseY, 0, windowHeight, -100, 100);
 
 
     //The following conditionals check for left right walls
-    if(rectangle.x > windowWidth){
+    if(eli.x > windowWidth){
         updateColors();
-        
-        rectangle.speed = rectangle.speed * -1;
+        eli.speed = eli.speed * -1;
 
     }
-    else if (rectangle.x < 0){
+    else if (eli.x < 0){
         updateColors();
-        rectangle.speed = rectangle.speed * -1;
+        eli.speed = eli.speed * -1;
     }
 
     //The following conditionals check for top bottom walls
-    if(rectangle.y > windowHeight){
+    if(eli.y > windowHeight){
         updateColors();
-        rectangle.speedy = rectangle.speedy * -1;
+        eli.speedy = eli.speedy * -1;
     }
-    else if (rectangle.y < 0 ){
+    else if (eli.y < 0 ){
         updateColors();
-        rectangle.speedy = rectangle.speedy * -1;
+        eli.speedy = eli.speedy * -1;
     }
     
     //update x and y location
-    rectangle.x = rectangle.x + rectangle.speed;
-    rectangle.y = rectangle.y + rectangle.speedy;
+    eli.x = eli.x + eli.speed;
+    eli.y = eli.y + eli.speedy;
 
-    
+  
 
-    //draws rectangle
+    //let diam = 100 + sin(theta) * maxDiameter ;
+    //draws eli
     if(x % 2 == 0){
-        fill(rectangle.color1,rectangle.color2,rectangle.color3);
+        fill(eli.color1,eli.color2,eli.color3, 5);
+        let diam = 100 + sin(theta) * windowWidth ;
+
+        // draw the circle 
+        ellipse(windowWidth/2,windowHeight/2, diam, diam); 
+    
+        // make theta keep getting bigger
+        // you can play with this number to change the speed
+        theta += .01; 
+
+        fill(eli.color1,eli.color2,eli.color3);
     }
     else{
-        fill(rectangle.color3,rectangle.color4,rectangle.color5);
-    }
+        fill(eli.color4,eli.color5,eli.color6, 5);
+        let diam = 100 + sin(theta) * windowHeight ;
+
+        // draw the circle 
+        ellipse(windowWidth/2,windowHeight/2, diam, diam); 
     
-    ellipse(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        // make theta keep getting bigger
+        // you can play with this number to change the speed
+        theta += .01; 
+
+        fill(eli.color4,eli.color5,eli.color6);
+    }
+
+    ellipse(eli.x, eli.y, eli.width, eli.height);
     x++;
+
+
+
 
     
 }
@@ -88,4 +113,5 @@ function draw(){
 function mouseClicked() {
     fill(70, 103, 80, 10)
     rect(0, 0, windowWidth, windowHeight);
+
 }
