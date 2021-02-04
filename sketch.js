c077e2a3648601d6bb60ba37ca433e372057c5f9
anaran7 <naranjoa101@gmail.com>
2021-02-04 13:05:44 -0600

    Updated to have user add more types of shapes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
+/*
+I am working with tempo of the expanding and contrasting of the circles with two main circles
+in the middle having the same tempo with one just being bigger than the other one.
+Furthermore, when you add more shapes they will be smaller than the two main circles that speed up in tempo the more you add
+and with it you can create different shapes and geometry.
+
+Controls
+--Press 1(circle),2(long ellipse),3(combo of the 2) to add shapes at the current position of the mouse
+--Press 0 to stop the movement of the current added shapes having the blend into the background resseting the tempo when you add more cicles.
+
+
+*/
+
+
 //eli object
 let eli = {
+    // Used to change the color of the shapes
     x: 0,
     y: 0,
-    width: 10, 
-    height: 50,
     speed: 6,
     speedy: 6,
     color1: 0,
@@ -18,6 +31,8 @@ let x = 0;
 let maxDiameter; 
 let theta = 0; 
 let theta1 = 0;
+let theta2 = 0;
+let theta3 = 0;
 let ellis = [];
 
 function setup(){
@@ -38,18 +53,10 @@ function updateColors(){
 }
 
 function draw(){
-
-    //strokeWeight(2);
-    //stroke(noise)
-    //background("#ffffff01");
-    //noStroke();  
-
-    //the height and the width is decided by mouse input
-    eli.width = map(mouseX, 0, windowWidth, -20, 20);
-    eli.height = map(mouseY, 0, windowHeight, -100, 100);
+ 
 
 
-    //The following conditionals check for left right walls
+    //The following conditionals check for left right walls -- This updates color
     if(eli.x > windowWidth){
         updateColors();
         eli.speed = eli.speed * -1;
@@ -60,7 +67,7 @@ function draw(){
         eli.speed = eli.speed * -1;
     }
 
-    //The following conditionals check for top bottom walls
+    //The following conditionals check for top bottom walls -- This updates color
     if(eli.y > windowHeight){
         updateColors();
         eli.speedy = eli.speedy * -1;
@@ -76,17 +83,13 @@ function draw(){
 
   
 
-    //let diam = 100 + sin(theta) * maxDiameter ;
-    //draws eli
+    //draws the two main circles
     if(x % 2 == 0){
         fill(eli.color1,eli.color2,eli.color3, 5);
-        let diam = 100 + sin(theta) * windowWidth ;
+        let diam = 100 + sin(theta) * windowWidth+300 ;
 
         // draw the circle 
         ellipse(windowWidth/2,windowHeight/2, diam, diam); 
-    
-        // make theta keep getting bigger
-        // you can play with this number to change the speed
         theta += .01; 
 
         fill(eli.color1,eli.color2,eli.color3);
@@ -97,18 +100,14 @@ function draw(){
 
         // draw the circle 
         ellipse(windowWidth/2,windowHeight/2, diam, diam); 
-    
-        // make theta keep getting bigger
-        // you can play with this number to change the speed
         theta += .01; 
 
         fill(eli.color4,eli.color5,eli.color6);
     }
 
-    //ellipse(eli.x, eli.y, eli.width, eli.height);
     x++;
 
-
+    //displays the shapes you adedd
     for (let i = 0; i < ellis.length; i++) {
         ellis[i].display();
     }
@@ -116,14 +115,24 @@ function draw(){
     
 }
 
-function mouseClicked() {
-    //fill(70, 103, 80, 10)
-    //rect(0, 0, windowWidth, windowHeight);
-    //ellipse(eli.x, eli.y, eli.width, eli.height);
-    ellis.push(new Elli(mouseX, mouseY));
-}
+//checks for user input to add more shapes or fade them in into the background
+function keyTyped() {
+    if (key === '0') {
+        //remove objects from list
+        while(ellis.length != 0){
+            ellis.pop();
+        }
+    } else if (key === '1') {
+        ellis.push(new Elli(mouseX, mouseY));
+    } else if (key === '2') {
+        ellis.push(new Elli1(mouseX, mouseY));
+    } else if (key === '3') {
+        ellis.push(new Elli2(mouseX, mouseY));
+    }
+  }
 
 
+//class to create the pulsing circle
 class Elli {
   constructor(xv, yv) {
     this.x = xv;
@@ -133,13 +142,49 @@ class Elli {
 
 
   display() {
+    //draws circle that pulses
     fill(eli.color1,eli.color5,eli.color3, 5);
-    let diam = 100 + sin(theta1) * 500 ;
-
-    // draw the circle 
+    let diam = 100 + sin(theta1) * 500 ; 
     ellipse(this.x,this.y, diam, diam); 
-    // make theta keep getting bigger
-    // you can play with this number to change the speed
     theta1 += .01; 
   }
+}
+
+
+//class to create the pulsing long ellipse
+class Elli1 {
+    constructor(xv, yv) {
+        this.x = xv;
+        this.y = yv;     
+    }
+  
+  
+    display() {
+      //draws circle that pulses
+      fill(eli.color1,eli.color5,eli.color3, 5);
+      let diam = 100 + sin(theta1) * 500 ;
+      let diam1 = 100 + sin(0) * 200 ; 
+      ellipse(this.x,this.y, diam, diam1); 
+      theta1 += .01; 
+    }
+}
+
+//class to create the pulsing combo of ellipse and circle
+class Elli2 {
+    constructor(xv, yv) {
+      this.x = xv;
+      this.y = yv;
+     
+    }
+  
+  
+    display() {
+      //draws circle that pulses
+      fill(eli.color1,eli.color5,eli.color3, 5);
+      let diam = 100 + sin(theta1) * 500 ;
+      let diam1 = 100 + sin(theta2) * 250 ; 
+      ellipse(this.x,this.y, diam, diam1); 
+      theta1 += .01; 
+      theta2 += .05
+    }
 }
